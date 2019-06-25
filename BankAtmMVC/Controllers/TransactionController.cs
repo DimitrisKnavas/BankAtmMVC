b29fe9080670bc61041d389b411bb8825a2728b1
Δημήτρης <dimitriskarn94@gmail.com>
2019-06-25 23:01:42 +0300

    Custom Identity data,Transaction controller and deposit action added

---
+++ BankAtmMVC/Controllers/TransactionController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using BankAtmMVC.Data;
+using BankAtmMVC.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace BankAtmMVC.Controllers
+{
+    [Authorize]
+    public class TransactionController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public TransactionController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: Transaction/Deposit
+        public IActionResult Deposit()
+        {
+            return View();
+        }
+
+        //GET: Transaction/Deposit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Deposit([Bind("Amount,Date")]Transaction transaction)
+        {
+            var currentId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            transaction.BankUserID = currentId;
+            try{
+
+                if (ModelState.IsValid)
+                {
+
+                  _context.Transactions.Add(transaction);
+                  await _context.SaveChangesAsync();
+                  return RedirectToAction("Index", "Home");
+                }
+            }
+            catch (DbUpdateException)
+            {
+             ModelState.AddModelError("", "Unable to complete the transaction. " +
+             "Try again, and if the problem persists " +
+            "see your system administrator.");
+            }
+
+             return View(transaction);
+        }
+    }
+}
---
+++ BankAtmMVC/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankAtmMVC.Models
+{
+    public class Transaction
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime Date { get; set; }
+
+
+        public string BankUserID { get; set; }
+        public BankUser AspNetUsers { get; set; }
+    }
+}
